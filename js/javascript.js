fd21ce7586c84dd3e07a87dab0c006b3bc96258a
kolarikjan <58089918+kolarikjan@users.noreply.github.com>
2021-09-21 00:00:35 +0200

    Homepage progress 2

--- js/javascript.js
+++ js/javascript.js
@@ -4,6 +4,26 @@ $(document).ready(function() {
         margin:0,
         nav:true,
         dots:true,
-        items:1
-    })
+        items:1,
+        navText: [ '<img src="icons/hp_banner-left.svg" class="bannerPrev">', '<img src="icons/hp_banner-right.svg" class="bannerNext">' ],
+    });
+    $('.firmy-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        dots:false,
+        items:5,
+        stagePadding: 275,
+        responsive:{
+            0:{
+                items:3,
+            },
+            993:{
+                items:4,
+            },
+            1200:{
+                items:4,
+            }
+        }
+    });
 });
